699bc378a4309fed0446b17eda774b13369298d3
Anirban <anirbanroymukherjee@outlook.com>
2020-10-20 22:14:50 +0530

    location circle done

--- app/src/main/java/com/bawarchef/android/DashboardUserActivity.java
+++ app/src/main/java/com/bawarchef/android/DashboardUserActivity.java
@@ -6,21 +6,26 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bawarchef.Communication.EncryptedPayload;
 import com.bawarchef.Communication.Message;
 import com.bawarchef.Communication.ObjectByteCode;
 import com.bawarchef.Containers.ChefIdentity;
+import com.bawarchef.Containers.GeoLocationCircle;
 import com.bawarchef.Containers.UserIdentity;
 import com.bawarchef.android.Fragments.Broadcasts;
 import com.bawarchef.android.Fragments.FoodMenu;
 import com.bawarchef.android.Fragments.History;
 import com.bawarchef.android.Fragments.Home;
+import com.bawarchef.android.Fragments.MessageReceiver;
 import com.bawarchef.android.Fragments.MyProfile;
 import com.bawarchef.android.Fragments.Orders;
 import com.bawarchef.android.Fragments.PersonalDetails;
@@ -31,6 +36,8 @@ import com.bawarchef.android.Fragments.UOrders;
 import com.bawarchef.android.Fragments.UPreferences;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class DashboardUserActivity extends AppCompatActivity {
 
@@ -43,8 +50,6 @@ public class DashboardUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
-
-        new Thread(() -> ((ThisApplication)getApplication()).startLocationUpdates(onLocationChange)).start();
 
         drawer = findViewById(R.id.drawer);
         menu_but = findViewById(R.id.menu_but);
@@ -68,40 +73,82 @@ public class DashboardUserActivity extends AppCompatActivity {
             setFragment(item);
             return true;
         });
-
     }
 
+    MobileClient.MessageProcessor defaultMessageProcessor = null;
+    MobileClient.MessageProcessor activityMessageProcessor = new MobileClient.MessageProcessor() {
+        @Override
+        public void process(Message m) {
+
+            if(m.getMsg_type().equals("GEOLOC_RESP")){
+                ArrayList<GeoLocationCircle> circles = (ArrayList<GeoLocationCircle>) m.getProperty("NEARBY_P");
+                GeoLocationCircle nearest = circles.get(0);
+                LatLng currL = ((ThisApplication)getApplication()).getLocation();
+                double min = nearest.getDist(currL.latitude,currL.longitude);
+                for (GeoLocationCircle loc : circles){
+                    double t = loc.getDist(currL.latitude,currL.longitude);
+                    if(t<min){
+                        min = t;
+                        nearest = loc;
+                    }
+                }
+                area_circ.setText(nearest.getPlaceTitle());
+            }
+
+            else{
+                ((MessageReceiver)activeFragment).process(m);
+            }
+
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        defaultMessageProcessor = ((ThisApplication)getApplication()).getMessageProcessor();
+        ((ThisApplication)getApplication()).setMessageProcessor(activityMessageProcessor);
+        ((ThisApplication)getApplication()).setCurrentContext(this);
+        new Thread(() -> ((ThisApplication)getApplication()).startLocationUpdates(onLocationChange)).start();
+    }
+
+    Fragment activeFragment = null;
+
     private void setFragment(MenuItem item) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Fragment fragment = null;
+        activeFragment = null;
 
         switch(item.getTitle().toString()){
 
             case "Home":
-                fragment = new UHome();
+                activeFragment = new UHome();
                 break;
 
             case "My Profile":
-                fragment = new UMyProfile();
+                activeFragment = new UMyProfile();
                 break;
 
             case "My Orders":
-                fragment = new UOrders();
+                activeFragment = new UOrders();
                 break;
 
             case "Preferences":
-                fragment = new UPreferences();
+                activeFragment = new UPreferences();
                 break;
 
             case "Quit":
                 finishAndRemoveTask();
                 break;
         }
-        if(fragment!=null)
+        if(activeFragment!=null)
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragmentViewPort,fragment)
+                    .replace(R.id.fragmentViewPort,activeFragment)
                     .commit();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ((ThisApplication)getApplication()).setMessageProcessor(defaultMessageProcessor);
     }
 
     @Override
